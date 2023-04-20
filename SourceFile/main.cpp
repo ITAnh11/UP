2d6a001e65744f41108a493777ae004291e2884f
ITAnh11 <buianhkc112004@gmail.com>
2023-04-20 15:36:41 +0700

    1.1 Name game window

--- SourceFile/main.cpp
+++ SourceFile/main.cpp
@@ -33,7 +33,7 @@ bool init()
         }
 
         // Create window
-        gWindow = SDL_CreateWindow("Super Memory", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        gWindow = SDL_CreateWindow("UP", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if (gWindow == NULL)
         {
             printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
